262cdf0160cf7ee002783f0f8792f60eb33d612b
Wizzorb <57352516+Wizzorb@users.noreply.github.com>
2020-12-23 14:51:59 +0000

    UML Base Code
    This is the base code for the project, generated from the class diagram created for the project

---
+++ COMP2000 Assessment/src/cardPayment.java
@@ -0,0 +1,10 @@
+
+public class cardPayment {
+    public paymentMethod pMethod;
+
+    public void verificationScreen() {
+//begin of modifiable zone(JavaCode)......C/c81307f5-c08c-4d0b-9045-ec180cdf773e
+
+//end of modifiable zone(JavaCode)........E/c81307f5-c08c-4d0b-9045-ec180cdf773e
+    }
+}
