f47ae987f1db03490b60c8923e01c5acf28704dd
Wizzorb <57352516+Wizzorb@users.noreply.github.com>
2021-01-01 16:59:55 +0000

    Optimised reading the database, added customer screen, added item scanning
    Additionally the two obsolete view forms were removed, no longer needed with current MVC setup

--- COMP2000 Assessment/src/View/KioskUI.java
+++ COMP2000 Assessment/src/View/KioskUI.java
@@ -1,23 +1,78 @@
 package View;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import Controller.automatedCheckoutSystem;
+import Model.stockDatabase;
 
-public class KioskUI {
-    private JPanel loginScreen;
+public class KioskUI extends JFrame {
+    private JPanel kioskPanel;
+    private JPanel loginPanel;
     private JButton adminButton;
     private JButton customerButton;
+    private JPanel adminPanel;
+    private JTextArea dBTextArea;
+    private JButton backButton;
+    private JButton newOrderButton;
+    private JPanel customerPanel;
+    private JList basketContent;
+    private JButton scanButton;
+    private JButton searchItemButton;
+    private JButton checkoutButton;
+    private JButton increaseButton;
+    private JButton decreaseButton;
+    private JButton backButton1;
+
+    stockDatabase kioskDB = new stockDatabase();
+    ArrayList<String> kioskList = new ArrayList<>();
+    ArrayList<String> basketList = new ArrayList<>();
 
     //This is a View in the MVC pattern.
-    public class kioskUI extends JFrame {
-        //public List<Controller.automatedCheckoutSystem> automated Checkout System = new ArrayList<Controller.automatedCheckoutSystem> ();
+    public KioskUI(String title) {
+        super(title);
+        setContentPane(kioskPanel);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setPreferredSize(new Dimension(500, 500));
+        pack();
 
-        //public receipt receipt;
+        adminButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                switchPanel(kioskPanel, "adminCard");
+                dBTextArea.setLineWrap(true);
+                //kioskDB.readStock(kioskList);
+                automatedCheckoutSystem.dBRead(kioskList);
+                dBTextArea.setText(kioskList.toString());
+            }
+        });
 
-        public void displayStockList() {
-//begin of modifiable zone(JavaCode)......C/4b6893f2-936d-4d74-8cf7-ce361e70aebf
+        customerButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                switchPanel(kioskPanel, "customerCard");
+            }
+        });
 
-//end of modifiable zone(JavaCode)........E/4b6893f2-936d-4d74-8cf7-ce361e70aebf
-        }
+        scanButton.addActionListener((new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String scanInsert = JOptionPane.showInputDialog(customerPanel, "Please scan purchase ID below.", null);
+                Integer insertID = Integer.parseInt(scanInsert);
+                kioskDB.readStock();
+                kioskDB.searchDB(insertID);
+                basketList.add(kioskDB.sID, kioskDB.sID.toString());
+                basketList.add(kioskDB.sID+1, kioskDB.sName);
+                basketList.add(kioskDB.sID+2, kioskDB.sPrice.toString());
+                basketList.add(kioskDB.sID+3, kioskDB.sQuantity.toString());
+            }
+        }));
+    }
 
+    public void switchPanel(Container container, String panelName) {
+        CardLayout card = (CardLayout) (container.getLayout());
+        card.show(container, panelName);
     }
 }
--- COMP2000 Assessment/src/com/company/Main.java
+++ COMP2000 Assessment/src/com/company/Main.java
@@ -5,7 +5,7 @@ import View.*;
 public class Main {
 
     public static void main(String[] args) {
-        AdminUI mainPage = new AdminUI("MainPage");
-        mainPage.setVisible(true);
+        KioskUI kioskPage = new KioskUI("KioskPage");
+        kioskPage.setVisible(true);
     }
 }
